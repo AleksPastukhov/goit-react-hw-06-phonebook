e5f849643d71102e2175df25d782cc3cfa313c3c
AleksPastukhov <passandro1990@gmail.com>
2023-03-18 19:31:36 +0200

    adds redux

--- src/index.js
+++ src/index.js
@@ -3,11 +3,15 @@ import ReactDOM from 'react-dom/client';
 import App from './components/App/App';
 import { theme } from './contans/Thema';
 import { ThemeProvider } from '@emotion/react';
+import { store } from './redux/store';
+import { Provider } from 'react-redux';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </ThemeProvider>
   </React.StrictMode>
 );
---
+++ src/redux/store.js
@@ -0,0 +1,21 @@
+import { configureStore } from '@reduxjs/toolkit';
+import { createAction, createReducer } from '@reduxjs/toolkit';
+
+const increment = createAction('filter/increment');
+
+console.log(increment('sdsdv'));
+
+const contactsReduser = createReducer([], {});
+const filterReduser = createReducer('', {});
+
+export const store = configureStore({
+  reducer: {
+    contacts: contactsReduser,
+    filter: filterReduser,
+  },
+});
+
+// const appState = {
+//   contacts: [],
+//   filter: '',
+// };
